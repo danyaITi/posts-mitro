67ebdf3a61e73ef11e5d915b3fe46e4c76547b90
danyaITi <101832155+danyaITi@users.noreply.github.com>
2023-05-30 19:23:05 +0000

    added prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
-import { RoutesApp } from "./routes";
-import "./styles.css";
+import { Link } from 'react-router-dom';
+import { RoutesApp } from './routes';
+import './styles.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/about"}>About</Link>
-      </nav>
-      <RoutesApp />
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/about'}>About</Link>
+            </nav>
+            <RoutesApp />
+        </div>
+    );
 };
 
 export default App;

8502249d79998c6857d308ef0444ec6d6704623c
danyaITi <101832155+danyaITi@users.noreply.github.com>
2023-06-04 19:34:31 +0000

    fix conflict

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -2,20 +2,20 @@ import { getPostsPending } from '../store/reducers/postsByIdSlice';
 import { useStoreDispatch } from '../store/store';
 import { Post } from '../store/reducers/postsSlice';
 import { useNavigate } from 'react-router-dom';
-=
-interbf0ceC7afdostProps<T> {
-    it
-em: T
+
+interface CardPostProps<T> {
+    item: T;
 }
+ 
+const CardPost = <T extends Post>({ item }: CardPostProps<T>) => {
+    const dispatch = useStoreDispatch();
+    const navigate = useNavigate();
 
-c8st CardPos31= <T ext nds Post>}: CardPostProps<T>) => {
-   in const dispatch = useStoreDispatch();
-    c9b4f 27829baacf908f3t0f7892f1aac802
-
-   in const handlePostsById = (userId: number) => {
+    const handlePostsById = (userId: number) => {
         dispatch(getPostsPending({ userId } as any));
         navigate(`user/${userId}`);
     };
+    
     return (
         <li className="mt-5">
             <img
@@ -27,5 +27,9 @@ c8st CardPos31= <T ext nds Post>}: CardPostProps<T>) => {
             <b>{item.title}</b>
             <p>{item.body}</p>
             <button className="p-1">Комментарии</button>
-       
-rdPostrt default CardPostt default CardPost;
+        </li>
+    );
+};
+
+export default CardPoststst</ti>
+ tlfrfr) tlfrfr)
